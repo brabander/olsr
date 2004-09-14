add49ab7aa4f7a1498096e77ee1c8981ef873887
Andreas Tonnesen <andreto@olsr.org>
2004-09-14 17:32:57 +0000

    The IPC socket now has the SO_REUSEADDR option set.

--- lib/dot_draw/src/olsrd_plugin.h
+++ lib/dot_draw/src/olsrd_plugin.h
@@ -57,7 +57,7 @@
  *****************************************************************************/
 
 #define PLUGIN_NAME    "OLSRD dot draw plugin"
-#define PLUGIN_VERSION "0.1"
+#define PLUGIN_VERSION "0.2"
 #define PLUGIN_AUTHOR   "Andreas Tønnesen"
 #define MOD_DESC PLUGIN_NAME " " PLUGIN_VERSION " by " PLUGIN_AUTHOR
 #define PLUGIN_INTERFACE_VERSION 1
