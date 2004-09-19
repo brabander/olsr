d861ee1baabeea3a176beb9c88d41dce3b68f1a9
Andreas Tonnesen <andreto@olsr.org>
2004-09-19 11:48:20 +0000

    Replaced unik-olsrd with olsr.org

--- gui/linux-gtk/src/ipc.h
+++ gui/linux-gtk/src/ipc.h
@@ -2,7 +2,7 @@
  * OLSR ad-hoc routing table management protocol GUI front-end
  * Copyright (C) 2003 Andreas Tønnesen (andreto@ifi.uio.no)
  *
- * This file is part of olsrd-unik.
+ * This file is part of olsr.org.
  *
  * uolsrGUI is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with olsrd-unik; if not, write to the Free Software
+ * along with olsr.org; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
