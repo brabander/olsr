7ed9a21d6b683b6ae2908f5b73c6cbaf1d3141bb
Andreas Tonnesen <andreto@olsr.org>
2004-09-21 19:02:56 +0000

    Added CVS Id fields to all ca nd h files

--- src/net.c
+++ src/net.c
@@ -18,11 +18,11 @@
  * You should have received a copy of the GNU General Public License
  * along with olsr.org; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ * 
+ * $ Id $
  *
  */
-
-/* $Id: net.c,v 1.8 2004/09/21 18:24:39 kattemat Exp $ */
-
 
 #include "net.h"
 #include "olsr.h"
