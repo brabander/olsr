5e941f87f81a95c0aba3447e1c44719e2ea08571
Hannes Gredler <hannes@gredler.at>
2008-02-19 22:58:52 +0100

    From Clemens Hopfer <datacop@inode.at>: dot_draw plugin: close the connection after graph output.

--- lib/dot_draw/src/olsrd_dot_draw.c
+++ lib/dot_draw/src/olsrd_dot_draw.c
@@ -267,6 +267,7 @@ ipc_action(int fd __attribute__((unused)))
     return;
   }
   olsr_printf(1, "(DOT DRAW)IPC: Connection from %s\n", inet_ntoa(pin.sin_addr));
+  close(ipc_connection); /* close connection after one output */
   pcf_event(1, 1, 1);
 }
 
