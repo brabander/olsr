a7ca59ffeff67e77a1391759cde44d5228d0375a
Sven-Ola Tuecke <sven-ola@gmx.de>
2008-06-12 08:35:11 +0200

    Win32 Fixes and disable LQWinSize input

--- src/tc_set.c
+++ src/tc_set.c
@@ -750,12 +750,23 @@ olsr_calculate_tc_border(olsr_u8_t lower_border,
 
     lower_border--;
     for (i = 0; i < lower_border / 8; i++) {
-      lower_border_ip->v6.in6_u.u6_addr8[olsr_cnf->ipsize - i - 1] = 0;
+#ifdef WIN32
+		lower_border_ip->v6.s6_addr[olsr_cnf->ipsize - i - 1] = 0;
+#else
+		lower_border_ip->v6.in6_u.u6_addr8[olsr_cnf->ipsize - i - 1] = 0;
+#endif
     }
+#ifdef WIN32
+    lower_border_ip->v6.s6_addr[olsr_cnf->ipsize - lower_border / 8 -
+				       1] &= (0xff << (lower_border & 7));
+    lower_border_ip->v6.s6_addr[olsr_cnf->ipsize - lower_border / 8 -
+				       1] |= (1 << (lower_border & 7));
+#else
     lower_border_ip->v6.in6_u.u6_addr8[olsr_cnf->ipsize - lower_border / 8 -
 				       1] &= (0xff << (lower_border & 7));
     lower_border_ip->v6.in6_u.u6_addr8[olsr_cnf->ipsize - lower_border / 8 -
 				       1] |= (1 << (lower_border & 7));
+#endif
   }
 
   if (upper_border == 0xff) {
@@ -766,12 +777,23 @@ olsr_calculate_tc_border(olsr_u8_t lower_border,
     upper_border--;
 
     for (i = 0; i < upper_border / 8; i++) {
-      upper_border_ip->v6.in6_u.u6_addr8[olsr_cnf->ipsize - i - 1] = 0;
+#ifdef WIN32
+		upper_border_ip->v6.s6_addr[olsr_cnf->ipsize - i - 1] = 0;
+#else
+		upper_border_ip->v6.in6_u.u6_addr8[olsr_cnf->ipsize - i - 1] = 0;
+#endif
     }
+#ifdef WIN32
+    upper_border_ip->v6.s6_addr[olsr_cnf->ipsize - upper_border / 8 -
+				       1] &= (0xff << (upper_border & 7));
+    upper_border_ip->v6.s6_addr[olsr_cnf->ipsize - upper_border / 8 -
+				       1] |= (1 << (upper_border & 7));
+#else
     upper_border_ip->v6.in6_u.u6_addr8[olsr_cnf->ipsize - upper_border / 8 -
 				       1] &= (0xff << (upper_border & 7));
     upper_border_ip->v6.in6_u.u6_addr8[olsr_cnf->ipsize - upper_border / 8 -
 				       1] |= (1 << (upper_border & 7));
+#endif
   }
   return 1;
 }
