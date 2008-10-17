fc22a73457e85a858dabbfc93c5b8612acb91e49
Bernd Petrovitsch <bernd@firmix.at>
2008-10-17 03:06:05 +0200

    Cleanup and bugfix the txtinfo plugin
    Contents:
    - fix file descriptor leaks if initialization find's an error
    - remove ipc_open and ipc_socket_up away. We use -1 in the respective
      file descriptors for "invalid".
    - killed unused variables and return types
    - killed trivially duplicated code
    - localized variable defintions
    - added a few "const"
    - removed superflous #include's
    - Some minor coding style issues.

--- lib/txtinfo/src/olsrd_txtinfo.c
+++ lib/txtinfo/src/olsrd_txtinfo.c
@@ -45,55 +45,30 @@
 /*
  * Dynamic linked library for the olsr.org olsr daemon
  */
-
  
-#include <sys/types.h>
-#include <sys/socket.h>
-#if !defined WIN32
-#include <sys/select.h>
-#endif
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/time.h>
-#include <time.h>
-#include <math.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
-#include <errno.h>
-
-#include "ipcalc.h"
+#include "olsrd_txtinfo.h"
 #include "olsr.h"
-#include "olsr_types.h"
+#include "ipcalc.h"
 #include "neighbor_table.h"
 #include "two_hop_neighbor_table.h"
 #include "mpr_selector_set.h"
 #include "tc_set.h"
 #include "hna_set.h"
 #include "mid_set.h"
-#include "link_set.h"
-#include "net_olsr.h"
-#include "lq_plugin.h"
+#include "routing_table.h"
 
-#include "olsrd_txtinfo.h"
-#include "olsrd_plugin.h"
-
+#include <errno.h>
+#include <stdarg.h>
+#include <unistd.h>
 
 #ifdef WIN32
 #define close(x) closesocket(x)
 #endif
 
 
-static int ipc_socket;
-static int ipc_open;
-static int ipc_connection;
-static int ipc_socket_up;
+static int ipc_socket = -1;
+static int ipc_connection = -1;
 
-
-/* IPC initialization function */
-static int plugin_ipc_init(void);
 
 static void send_info(int send_what);
 
@@ -122,7 +97,16 @@ static void ipc_print_mid(void);
 #define SIW_TOPO 6
 #define SIW_NEIGHLINK 7
 
-static int ipc_sendf(const char* format, ...) __attribute__((format(printf, 1, 2)));
+static void ipc_sendf(const char* format, ...) __attribute__((format(printf, 1, 2)));
+
+/**
+ * destructor - called at unload
+ */
+void olsr_plugin_exit(void)
+{
+    CLOSE(ipc_socket);
+    CLOSE(ipc_connection);
+}
 
 /**
  *Do initialization here
@@ -133,103 +117,81 @@ static int ipc_sendf(const char* format, ...) __attribute__((format(printf, 1, 2
 int
 olsrd_plugin_init(void)
 {
-    /* Initial IPC value */
-    ipc_open = 0;
-    ipc_socket_up = 0;
-
-    plugin_ipc_init();
-    return 1;
-}
-
-
-/**
- * destructor - called at unload
- */
-void olsr_plugin_exit(void)
-{
-    if(ipc_open)
-        close(ipc_socket);
-}
-
-
-
-static int
-plugin_ipc_init(void)
-{
     struct sockaddr_storage sst;
-    struct sockaddr_in *addr;
-    struct sockaddr_in6 *addr6;
     olsr_u32_t yes = 1;
     socklen_t addrlen;
 
     /* Init ipc socket */
-    if ((ipc_socket = socket(olsr_cnf->ip_version, SOCK_STREAM, 0)) == -1) {
+    ipc_socket = socket(olsr_cnf->ip_version, SOCK_STREAM, 0);
+    if (ipc_socket == -1) {
 #ifndef NODEBUG
         olsr_printf(1, "(TXTINFO) socket()=%s\n", strerror(errno));
 #endif
         return 0;
-    } else {
-        if (setsockopt(ipc_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&yes, sizeof(yes)) < 0) {
+    }
+    if (setsockopt(ipc_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&yes, sizeof(yes)) < 0) {
 #ifndef NODEBUG
-            olsr_printf(1, "(TXTINFO) setsockopt()=%s\n", strerror(errno));
+        olsr_printf(1, "(TXTINFO) setsockopt()=%s\n", strerror(errno));
 #endif
-            return 0;
-        }
+        CLOSE(ipc_socket);
+        return 0;
+    }
 
 #if defined __FreeBSD__ && defined SO_NOSIGPIPE
-        if (setsockopt(ipc_socket, SOL_SOCKET, SO_NOSIGPIPE, (char *)&yes, sizeof(yes)) < 0) {
-            perror("SO_REUSEADDR failed");
-            return 0;
-        }
+    if (setsockopt(ipc_socket, SOL_SOCKET, SO_NOSIGPIPE, (char *)&yes, sizeof(yes)) < 0) {
+        perror("SO_REUSEADDR failed");
+        CLOSE(ipc_socket);
+        return 0;
+    }
 #endif
-        /* Bind the socket */
+    /* Bind the socket */
 
-        /* complete the socket structure */
-        memset(&sst, 0, sizeof(sst));
-        if (olsr_cnf->ip_version == AF_INET) {
-           addr = (struct sockaddr_in *)&sst;
-           addr->sin_family = AF_INET;
-           addrlen = sizeof(struct sockaddr_in);
+    /* complete the socket structure */
+    memset(&sst, 0, sizeof(sst));
+    if (olsr_cnf->ip_version == AF_INET) {
+        struct sockaddr_in *addr4 = (struct sockaddr_in *)&sst;
+        addr4->sin_family = AF_INET;
+        addrlen = sizeof(struct sockaddr_in);
 #ifdef SIN6_LEN
-           addr->sin_len = addrlen;
+        addr4->sin_len = addrlen;
 #endif
-           addr->sin_addr.s_addr = INADDR_ANY;
-           addr->sin_port = htons(ipc_port);
-        } else {
-           addr6 = (struct sockaddr_in6 *)&sst;
-           addr6->sin6_family = AF_INET6;
-           addrlen = sizeof(struct sockaddr_in6);
+        addr4->sin_addr.s_addr = INADDR_ANY;
+        addr4->sin_port = htons(ipc_port);
+    } else {
+        struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *)&sst;
+        addr6->sin6_family = AF_INET6;
+        addrlen = sizeof(struct sockaddr_in6);
 #ifdef SIN6_LEN
-           addr6->sin6_len = addrlen;
+        addr6->sin6_len = addrlen;
 #endif
-           addr6->sin6_addr = in6addr_any;
-           addr6->sin6_port = htons(ipc_port);
-        }
+        addr6->sin6_addr = in6addr_any;
+        addr6->sin6_port = htons(ipc_port);
+    }
       
-        /* bind the socket to the port number */
-        if (bind(ipc_socket, (struct sockaddr *) &sst, addrlen) == -1) {
+    /* bind the socket to the port number */
+    if (bind(ipc_socket, (struct sockaddr *) &sst, addrlen) == -1) {
 #ifndef NODEBUG
-            olsr_printf(1, "(TXTINFO) bind()=%s\n", strerror(errno));
+        olsr_printf(1, "(TXTINFO) bind()=%s\n", strerror(errno));
 #endif
-            return 0;
-        }
+        CLOSE(ipc_socket);
+        return 0;
+    }
 
-        /* show that we are willing to listen */
-        if (listen(ipc_socket, 1) == -1) {
+    /* show that we are willing to listen */
+    if (listen(ipc_socket, 1) == -1) {
 #ifndef NODEBUG
-            olsr_printf(1, "(TXTINFO) listen()=%s\n", strerror(errno));
+        olsr_printf(1, "(TXTINFO) listen()=%s\n", strerror(errno));
 #endif
-            return 0;
-        }
+        CLOSE(ipc_socket);
+        return 0;
+    }
 
-        /* Register with olsrd */
-        add_olsr_socket(ipc_socket, &ipc_action, NULL, NULL, SP_PR_READ);
+    /* Register with olsrd */
+    add_olsr_socket(ipc_socket, &ipc_action, NULL, NULL, SP_PR_READ);
         
 #ifndef NODEBUG
-        olsr_printf(2, "(TXTINFO) listening on port %d\n",ipc_port);
+    olsr_printf(2, "(TXTINFO) listening on port %d\n",ipc_port);
 #endif
-        ipc_socket_up = 1;
-    }
     return 1;
 }
 
@@ -237,19 +199,18 @@ plugin_ipc_init(void)
 static void ipc_action(int fd, void *data __attribute__((unused)), unsigned int flags __attribute__((unused)))
 {
     struct sockaddr_storage pin;
-    struct sockaddr_in *addr4;
-    struct sockaddr_in6 *addr6;
     char addr[INET6_ADDRSTRLEN];
     fd_set rfds;
     struct timeval tv;
-    int send_what = 0;
+    int send_what;
+    socklen_t addrlen;
 
-    socklen_t addrlen = sizeof(struct sockaddr_storage);
-
-    if(ipc_open)
+    if (ipc_connection >= 0) {
         return;
-
-    if ((ipc_connection = accept(fd, (struct sockaddr *)  &pin, &addrlen)) == -1) {
+    }
+    addrlen = sizeof(struct sockaddr_storage);
+    ipc_connection = accept(fd, (struct sockaddr *)&pin, &addrlen);
+    if (ipc_connection == -1) {
 #ifndef NODEBUG
         olsr_printf(1, "(TXTINFO) accept()=%s\n", strerror(errno));
 #endif
@@ -258,17 +219,17 @@ static void ipc_action(int fd, void *data __attribute__((unused)), unsigned int 
 
     tv.tv_sec = tv.tv_usec = 0;
     if (olsr_cnf->ip_version == AF_INET) {
-        addr4 = (struct sockaddr_in *)&pin;
+        const struct sockaddr_in *addr4 = (struct sockaddr_in *)&pin;
         if (inet_ntop(olsr_cnf->ip_version, &addr4->sin_addr, addr,
            INET6_ADDRSTRLEN) == NULL)
              addr[0] = '\0';
         if (!ip4equal(&addr4->sin_addr, &ipc_accept_ip.v4)) {
             olsr_printf(1, "(TXTINFO) From host(%s) not allowed!\n", addr);
-            close(ipc_connection);
+            CLOSE(ipc_connection);
             return;
         }
     } else {
-        addr6 = (struct sockaddr_in6 *)&pin;
+        const struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *)&pin;
         if (inet_ntop(olsr_cnf->ip_version, &addr6->sin6_addr, addr,
            INET6_ADDRSTRLEN) == NULL)
              addr[0] = '\0';
@@ -276,19 +237,19 @@ static void ipc_action(int fd, void *data __attribute__((unused)), unsigned int 
         if (!ip6equal(&in6addr_any, &ipc_accept_ip.v6) &&
            !ip6equal(&addr6->sin6_addr, &ipc_accept_ip.v6)) {
             olsr_printf(1, "(TXTINFO) From host(%s) not allowed!\n", addr);
-            close(ipc_connection);
+            CLOSE(ipc_connection);
             return;
         }
     }
-    ipc_open = 1;
 #ifndef NODEBUG
     olsr_printf(2, "(TXTINFO) Connect from %s\n",addr);
 #endif
       
+    send_what = 0;
     /* purge read buffer to prevent blocking on linux*/
     FD_ZERO(&rfds);
     FD_SET((unsigned int)ipc_connection, &rfds); /* Win32 needs the cast here */
-    if(0 <= select(ipc_connection+1, &rfds, NULL, NULL, &tv)) {
+    if (0 <= select(ipc_connection+1, &rfds, NULL, NULL, &tv)) {
         char requ[128];
         ssize_t s = recv(ipc_connection, (void*)&requ, sizeof(requ), 0); /* Win32 needs the cast here */
         if (0 < s) {
@@ -307,10 +268,9 @@ static void ipc_action(int fd, void *data __attribute__((unused)), unsigned int 
         }
     }
 
-	send_info(send_what);
+    send_info(send_what);
 	  
-    close(ipc_connection);
-    ipc_open = 0;
+    CLOSE(ipc_connection);
 }
 
 static void ipc_print_neigh(void)
@@ -332,12 +292,11 @@ static void ipc_print_neigh(void)
                   neigh->willingness);
         thop_cnt = 0;
 
-        for(list_2 = neigh->neighbor_2_list.next;
-            list_2 != &neigh->neighbor_2_list;
-            list_2 = list_2->next)
-        {
+        for (list_2 = neigh->neighbor_2_list.next;
+             list_2 != &neigh->neighbor_2_list;
+             list_2 = list_2->next) {
             //size += sprintf(&buf[size], "<option>%s</option>\n", olsr_ip_to_string(&buf1, &list_2->neighbor_2->neighbor_2_addr));
-            thop_cnt ++;
+            thop_cnt++;
         }
         ipc_sendf("%d\n", thop_cnt);
     } OLSR_FOR_ALL_NBR_ENTRIES_END(neigh);
@@ -346,15 +305,14 @@ static void ipc_print_neigh(void)
 
 static void ipc_print_link(void)
 {
-    struct ipaddr_str buf1, buf2;
-    struct lqtextbuffer lqbuffer1, lqbuffer2;
-
     struct link_entry *lnk;
 
     ipc_sendf("Table: Links\nLocal IP\tRemote IP\tHyst.\tLQ\tNLQ\tCost\n");
 
     /* Link set */
     OLSR_FOR_ALL_LINK_ENTRIES(lnk) {
+        struct ipaddr_str buf1, buf2;
+        struct lqtextbuffer lqbuffer1, lqbuffer2;
 	ipc_sendf( "%s\t%s\t%0.2f\t%s\t%s\t\n",
                    olsr_ip_to_string(&buf1, &lnk->local_iface_addr),
                    olsr_ip_to_string(&buf2, &lnk->neighbor_iface_addr),
@@ -368,14 +326,14 @@ static void ipc_print_link(void)
 
 static void ipc_print_routes(void)
 {
-    struct ipaddr_str buf1, buf2;
     struct rt_entry *rt;
-    struct lqtextbuffer lqbuffer;
     
     ipc_sendf("Table: Routes\nDestination\tGateway IP\tMetric\tETX\tInterface\n");
 
     /* Walk the route table */
     OLSR_FOR_ALL_RT_ENTRIES(rt) {
+        struct ipaddr_str buf1, buf2;
+        struct lqtextbuffer lqbuffer;
         ipc_sendf( "%s/%d\t%s\t%d\t%s\t%s\t\n",
                    olsr_ip_to_string(&buf1, &rt->rt_dst.prefix),
                    rt->rt_dst.prefix_len,
@@ -386,7 +344,6 @@ static void ipc_print_routes(void)
     } OLSR_FOR_ALL_RT_ENTRIES_END(rt);
 
     ipc_sendf("\n");
-
 }
 
 static void ipc_print_topology(void)
@@ -416,35 +373,23 @@ static void ipc_print_topology(void)
 
 static void ipc_print_hna(void)
 {
-    int size;
-    struct ip_prefix_list *hna;
+    const struct ip_prefix_list *hna;
     struct tc_entry *tc;
-    struct hna_net *tmp_net;
     struct ipaddr_str addrbuf, mainaddrbuf;
-
-    size = 0;
 
     ipc_sendf("Table: HNA\nDestination\tGateway\n");
 
     /* Announced HNA entries */
-    if (olsr_cnf->ip_version == AF_INET) {
-        for(hna = olsr_cnf->hna_entries; hna != NULL; hna = hna->next) {
-            ipc_sendf("%s/%d\t%s\n",
-                      olsr_ip_to_string(&addrbuf, &hna->net.prefix),
-                      hna->net.prefix_len,
-                      olsr_ip_to_string(&mainaddrbuf, &olsr_cnf->main_addr));
-        }
-    } else {
-        for(hna = olsr_cnf->hna_entries; hna != NULL; hna = hna->next) {
-            ipc_sendf("%s/%d\t%s\n",
-                      olsr_ip_to_string(&addrbuf, &hna->net.prefix),
-                      hna->net.prefix_len,
-                      olsr_ip_to_string(&mainaddrbuf, &olsr_cnf->main_addr));
-        }
+    for (hna = olsr_cnf->hna_entries; hna != NULL; hna = hna->next) {
+        ipc_sendf("%s/%d\t%s\n",
+                  olsr_ip_to_string(&addrbuf, &hna->net.prefix),
+                  hna->net.prefix_len,
+                  olsr_ip_to_string(&mainaddrbuf, &olsr_cnf->main_addr));
     }
 
     /* HNA entries */
     OLSR_FOR_ALL_TC_ENTRIES(tc) {
+        struct hna_net *tmp_net;
         /* Check all networks */
         OLSR_FOR_ALL_TC_HNA_ENTRIES(tc, tmp_net) {
 
@@ -461,24 +406,20 @@ static void ipc_print_hna(void)
 
 static void ipc_print_mid(void)
 {
-    unsigned short is_first;
     struct tc_entry *tc;
-    struct mid_entry *alias;
 
     ipc_sendf("Table: MID\nIP address\tAliases\n");
 
     /* MID root is the TC entry */
     OLSR_FOR_ALL_TC_ENTRIES(tc) {
         struct ipaddr_str buf;
-        ipc_sendf( olsr_ip_to_string(&buf,  &tc->addr ) );
-        is_first = 1;
+        struct mid_entry *alias;
+        char sep = '\t';
+        ipc_sendf("%s",  olsr_ip_to_string(&buf,  &tc->addr ) );
 
         OLSR_FOR_ALL_TC_MID_ENTRIES(tc, alias) {
-            ipc_sendf( "%s%s",
-                       ( is_first ? "\t" : ";" ),
-                       olsr_ip_to_string(&buf, &alias->mid_alias_addr));
-
-            is_first = 0;
+            ipc_sendf( "%c%s", sep, olsr_ip_to_string(&buf, &alias->mid_alias_addr));
+            sep = ';';
         }  OLSR_FOR_ALL_TC_MID_ENTRIES_END(tc, alias);
         ipc_sendf("\n");
     } OLSR_FOR_ALL_TC_ENTRIES_END(tc);
@@ -519,30 +460,28 @@ static void  send_info(int send_what)
  * scalable solution here.
  */
  
-static int  ipc_sendf(const char* format, ...)
+static void ipc_sendf(const char* format, ...)
 {
-    char txtnetbuf[TXT_IPC_BUFSIZE];
+    if (ipc_connection >= 0) {
+        char txtnetbuf[TXT_IPC_BUFSIZE];
 
-    va_list arg;
-    int rv;
+        va_list arg;
+        int rv;
 #if defined __FreeBSD__ || defined __NetBSD__ || defined __OpenBSD__ || defined __MacOSX__
-    int flags = 0;
+#define flags 0
 #else
-    int flags = MSG_NOSIGNAL;
+#define flags MSG_NOSIGNAL
 #endif
-    va_start(arg, format);
-    rv = vsnprintf(txtnetbuf, sizeof(txtnetbuf), format, arg);
-    va_end(arg);
-    if(ipc_socket_up) {
+        va_start(arg, format);
+        rv = vsnprintf(txtnetbuf, sizeof(txtnetbuf), format, arg);
+        va_end(arg);
         if (0 > send(ipc_connection, txtnetbuf, rv, flags)) {
 #ifndef NODEBUG
             olsr_printf(1, "(TXTINFO) Failed sending data to client!\n");
 #endif
-            close(ipc_connection);
-            return - 1;
+            CLOSE(ipc_connection);
         }
     }
-    return rv;
 }
 
 /*
