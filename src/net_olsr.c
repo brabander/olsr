3569d14f7f135a99e5035bcb47bc2fb63492c46a
Bernd Petrovitsch <bernd@firmix.at>
2008-11-10 00:25:11 +0100

    * killed an unused variable

--- src/net_olsr.c
+++ src/net_olsr.c
@@ -360,7 +360,6 @@ net_output(struct interface *ifp)
     const int save_errno = errno;
 #if 1
     char sabuf[1024];
-    struct ipaddr_str buf;
     dstaddr.sin.sa_family = olsr_cnf->ip_version;
     fprintf(stderr, "OLSR: sendto IPv%d: %s\n", olsr_cnf->ip_version == AF_INET ? 4 : 6, strerror(save_errno));
     fprintf(stderr, "To: %s (size: %d)\n", sockaddr_to_string(sabuf, sizeof(sabuf), &dstaddr.sin, dstaddr_size), dstaddr_size);
