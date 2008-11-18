19789baf30c00dc88f9a42119c41b1dbe1ccc1a2
Bernd Petrovitsch <bernd@firmix.at>
2008-11-18 23:07:17 +0100

    * whietspace and radability fixes

--- lib/httpinfo/src/olsrd_plugin.c
+++ lib/httpinfo/src/olsrd_plugin.c
@@ -105,14 +105,14 @@ static void my_fini(void)
 }
 
 static const struct olsrd_plugin_parameters plugin_parameters[] = {
-    { .name = "port",   .set_plugin_parameter = &set_plugin_port,      .data = &http_port },
+    { .name = "port",    .set_plugin_parameter = &set_plugin_port,     .data = &http_port },
     { .name = "host4",   .set_plugin_parameter = &add_plugin_ipaddr4,  .data = &allowed_nets },
     { .name = "net4",    .set_plugin_parameter = &add_plugin_ipnet4,   .data = &allowed_nets },
-    { .name = "host",   .set_plugin_parameter = &add_plugin_ipaddr4,   .data = &allowed_nets },
-    { .name = "net",    .set_plugin_parameter = &add_plugin_ipnet4,    .data = &allowed_nets },
+    { .name = "host",    .set_plugin_parameter = &add_plugin_ipaddr4,  .data = &allowed_nets },
+    { .name = "net",     .set_plugin_parameter = &add_plugin_ipnet4,   .data = &allowed_nets },
     { .name = "host6",   .set_plugin_parameter = &add_plugin_ipaddr6,  .data = &allowed_nets },
     { .name = "net6",    .set_plugin_parameter = &add_plugin_ipnet6,   .data = &allowed_nets },
-    { .name = "resolve",.set_plugin_parameter = &set_plugin_boolean,          .data = &resolve_ip_addresses },
+    { .name = "resolve", .set_plugin_parameter = &set_plugin_boolean,  .data = &resolve_ip_addresses },
 };
 
 void olsrd_get_plugin_parameters(const struct olsrd_plugin_parameters **params, int *size)
@@ -124,27 +124,35 @@ void olsrd_get_plugin_parameters(const struct olsrd_plugin_parameters **params, 
 static int insert_plugin_ipnet(sa_family_t ip_version, const char *sz_net, const char *sz_mask, struct ip_prefix_list **all_nets)
 {
     union olsr_ip_addr net;
-    union olsr_ip_addr netmask;
-    long prefix_len;
-
-    if(inet_pton(ip_version, sz_net, &net) <= 0) return 1;
-
-    if(ip_version == AF_INET) {
-      if(inet_pton(AF_INET, sz_mask, &netmask) <= 0) return 1;
-      prefix_len = olsr_netmask_to_prefix(&netmask);
+    olsr_u8_t prefix_len;
+    
+    if (inet_pton(ip_version, sz_net, &net) <= 0) {
+        return 1;
+    }
+    if (ip_version == AF_INET) {
+        union olsr_ip_addr netmask;
+        if(inet_pton(AF_INET, sz_mask, &netmask) <= 0) {
+            return 1;
+        }
+        prefix_len = olsr_netmask_to_prefix(&netmask);
     } else {
-      prefix_len = strtoul(sz_mask, NULL, 10);
-      if(prefix_len < 0 || prefix_len > 128) return 1;
+        char *endptr;
+        prefix_len = strtoul(sz_mask, &endptr, 10);
+        if(*endptr != '\0' || prefix_len < 0 || prefix_len > 128) {
+            return 1;
+        }
     }
 
-    if(ip_version != olsr_cnf->ip_version) {
-      if(ip_version == AF_INET) {
-        memmove(&net.v6.s6_addr[12], &net.v4.s_addr, sizeof(in_addr_t));
-        memset(&net.v6.s6_addr[0], 0x00, 10 * sizeof(uint8_t));
-        memset(&net.v6.s6_addr[10], 0xff, 2 * sizeof(uint8_t));
-        prefix_len += 96;
-      }
-      else return 1;
+    if (ip_version != olsr_cnf->ip_version) {
+        if (ip_version == AF_INET) {
+            memmove(&net.v6.s6_addr[12], &net.v4.s_addr, sizeof(in_addr_t));
+            memset(&net.v6.s6_addr[0], 0x00, 10 * sizeof(uint8_t));
+            memset(&net.v6.s6_addr[10], 0xff, 2 * sizeof(uint8_t));
+            prefix_len += 96;
+        }
+        else {
+            return 1;
+        }
     }
 
     ip_prefix_list_add(all_nets, &net, prefix_len);
