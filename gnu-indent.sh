917b7f2a576af801aa0f063f5090444dcc199112
Sven-Ola Tuecke <sven-ola@gmx.de>
2008-11-22 10:56:02 +0100

    Oops: fixed self modification of gnu-indent.sh

--- gnu-indent.sh
+++ gnu-indent.sh
@@ -22,7 +22,7 @@ EOF
   test "y" = "$l" || exit 1
 }
 
-sed -i 's/Andreas T.\{1,6\}nnesen/Andreas Tonnesen/g;s/Andreas Tønnesen/Andreas Tonnesen/g;s/Andreas TÃ¸nmnesen/Andreas Tonnesen/' $(find -type f -not -path "*/.hg*")
+sed -i 's/Andreas T.\{1,6\}nnesen/Andreas Tonnesen/g;s/Andreas Tønnesen/Andreas Tonnesen/g;s/Andreas TÃ¸nmnesen/Andreas Tonnesen/' $(find -type f -not -path "*/.hg*" -not -name ${0##*/})
 sed -i 's///g;s/[	 ]\+$//' $(find -name "*.[ch]" -not -path "*/.hg*")
 indent -sob -nhnl -nut $(find -name "*.[ch]" -not -path "*/.hg*")
 rm $(find -name "*~" -not -path "*/.hg*")
