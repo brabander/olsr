9dca734326d5459bd7ef07176d0a9cb52bf24a6c
Sven-Ola Tuecke <sven-ola@gmx.de>
2009-01-16 16:54:55 +0100

    win32verify: small fix

--- gui/win32/mk.sh
+++ gui/win32/mk.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+#CFLAGS="-O0 -ggdb -DDEBUG"
+CFLAGS="-O2 -DNODEBUG -DNDEBUG"
+
 SRCS="olsr_cfg olsr_cfg_gen olsr_ip_acl olsr_ip_prefix_list olsr_logging_data ipcalc builddata common/autobuf"
 OBJS=
 
 for i in $SRCS; do
-  gcc -Werror -Wall -I../../src -O0 -ggdb -DDEBUG -c ../../src/$i.c
+  gcc -Werror -Wall -I../../src $CFLAGS -c ../../src/$i.c
   OBJS="$OBJS ${i##*/}.o"
 done
-gcc -Werror -Wall -I../../src -O0 -ggdb -DDEBUG -o win32verify win32verify.c $OBJS
+gcc -Werror -Wall -I../../src $CFLAGS -o win32verify win32verify.c $OBJS
