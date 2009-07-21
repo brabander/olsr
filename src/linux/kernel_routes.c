9253d141578ddf792bab13889e567fd8bc24c611
Markus Kittenberger <Markus.Kittenberger@gmx.at>
2009-07-22 00:32:12 +0200

    rtnetlink: adopting error handling of Network unreachable to work with 2.6 kernels

--- src/linux/kernel_routes.c
+++ src/linux/kernel_routes.c
@@ -193,17 +193,19 @@ olsr_netlink_send(struct nlmsghdr *n, char *buf, size_t bufSize, uint8_t flag, c
                 rt_ret = 0;
               }
               /* insert route to gateway on the fly if "Network unreachable" (128) on 2.4 kernels
-               * or on 2.6 kernel No such process (3) is reported in rtnetlink response
+               * or on 2.6 kernel No such process (3), or Network unreachable (101) is reported in rtnetlink response
                * do this only with flat metric, as using metric values inherited from
                * a target behind the gateway is really strange, and could lead to multiple routes!
                * anyways if invalid gateway ips may happen we are f*cked up!!
                * but if not, these on the fly generated routes are no problem, and will only get used when needed
                * warning currently only for ipv4 */
-              else if (((errno == 3) || (errno == 128)) && (flag == RT_ORIG_REQUEST) && (FIBM_FLAT == olsr_cnf->fib_metric)
+              else if (((errno == 3) || (errno == 101) || (errno == 128)) && (flag == RT_ORIG_REQUEST) && (FIBM_FLAT == olsr_cnf->fib_metric)
                        && (n->nlmsg_type == RTM_NEWROUTE) && (rt->rt_dst.prefix.v4.s_addr != nexthop->gateway.v4.s_addr)) {
                 if (errno == 128)
                   OLSR_WARN(LOG_ROUTING, ". autogenerating route to handle 'Network unreachable' (128) while adding route!");
-                else
+                else if (errno == 128)
+		  OLSR_WARN(LOG_ROUTING, ". autogenerating route to handle 'Network unreachable' (101) while adding route!");
+		else
                   OLSR_WARN(LOG_ROUTING, ". autogenerating route to handle 'No such process' (3) while adding route!");
 
                 rt_ret = RT_AUTO_ADD_GATEWAY_ROUTE;     /* processing will contiune after this loop */
