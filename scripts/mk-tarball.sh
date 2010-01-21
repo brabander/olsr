200b3713e044c0fa5a76d82a3b7daf5c603d9542
Henning Rogge <henning.rogge@fkie.fraunhofer.de>
2010-01-21 08:07:14 +0100

    Improve makefile clean_libs code to prevent leftover from crosscompiling Add .project and .cproject to excludes in mk-tarball script

--- scripts/mk-tarball.sh
+++ scripts/mk-tarball.sh
@@ -50,7 +50,7 @@ mkdir /tmp/$NAME
 # clean stuff up first
 cd ..;make uberclean
 # sync the stuff to a working directory
-rsync -a . /tmp/$NAME/ --exclude=.hg* --exclude=.git* --exclude=*.rej --delete
+rsync -a . /tmp/$NAME/ --exclude=.project --exclude=.cproject --exclude=.hg* --exclude=.git* --exclude=*.rej --delete
 cd /tmp/
 echo "### creating /tmp/$NAME.tar.gz"
 tar -czf /tmp/$NAME.tar.gz $NAME
