b15ff98246dab3294e3b15b83b291af0c436d63b
Henning Rogge <hrogge@googlemail.com>
2010-05-03 17:29:18 +0200

    Lazy initialization for nameservice plugin (wait for main_ip)

--- src/ipcalc.h
+++ src/ipcalc.h
@@ -53,6 +53,8 @@ extern const struct olsr_ip_prefix ipv4_internet_route;
 extern const struct olsr_ip_prefix ipv6_mappedv4_route;
 extern const struct olsr_ip_prefix ipv6_internet_route;
 
+extern const union olsr_ip_addr olsr_ip_zero;
+
 extern const union olsr_ip_addr ipv6_def_multicast;
 
 struct ipaddr_str {
